9a4dfae4792c471eb7035b9f820324b33858e2b1
y0329 <kida_hirokazu@tuta.io>
2023-07-11 14:25:29 +0900

    初期状態

---
+++ block_breaker.py
@@ -0,0 +1,46 @@
+from typing import Any
+import pygame as pg
+import sys
+WIDTH, HEIGHT = 1600, 900
+class Block(pg.sprite.Sprite):
+    """
+    ブロックに関するクラス
+    """
+    def __init__(self,width:int,height:int,x:int,y:int):
+        super().__init__()
+        self.img = pg.Surface((width, height))
+        self.img.fill((255, 255, 255))  # 白色で塗りつぶす
+        self.rect = self.img.get_rect()
+        self.rect.center = x,y
+    def update(self,screen: pg.Surface):
+        screen.blit(self.img,self.rect)
+    
+
+def main():
+    pg.display.set_caption("ブロック崩し改")
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    blocks = pg.sprite.Group()
+    ball = pg.sprite.Group()
+    
+    # 初期ブロックの追加
+    for i in range(1,10):
+        for j in range(1,10):
+            blocks.add(Block(100, 25,200+(110*i),100+(30*j)))
+    
+    while True:
+        key_lst = pg.key.get_pressed()
+        
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return 0
+
+        # ブロックの更新と描画
+        blocks.update(screen)
+        
+        # 画面の更新
+        pg.display.flip()
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
